bec20fd68c9bc667d756af5d49172adef4dcf84c
logan <logancaldwell23@gmail.com>
2020-03-05 10:04:53 -0700

    starting the framework

---
+++ scses.sh
@@ -0,0 +1,34 @@
+bold=$(tput bold)
+normal=$(tput sgr0)
+echo
+
+#Default values for flags
+verbose='false'
+aflag='.txt'
+bflag=''
+input=.
+performance='false'
+
+#Case to get flags
+while getopts 'abi:vp' flag; do
+  case "${flag}" in
+    a) aflag='*' ;;
+    b) bflag='true' ;;
+    i) input="${OPTARG}" ;;
+    v) verbose='true' ;;
+    p) performance='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+
+#Variables you can adjust
+column1="%-20s" # spaces before column 2 starts
+column2="%-20s" # spaces before column 2 startsi
+
+
+
+
+
+
+
